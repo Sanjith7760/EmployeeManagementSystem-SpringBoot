587ec295392493f3f786b98180ca17753d0057ab
Sanjith7760 <sanjith.hangala@gmail.com>
2025-01-02 14:27:03 +0530

    Initial commit

---
+++ EmployeeManagement-REST/src/main/java/com/example/demo/service/EmployeeServiceImplementation.java
@@ -0,0 +1,44 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.EmployeeRepository;
+
+@Service
+public class EmployeeServiceImplementation implements EmployeeService{
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+	
+	@Override
+	public Employee createEmployee(Employee employee) {
+		return  employeeRepository.save(employee);
+	}
+
+	@Override
+	public List<Employee> fetchAllEmployees() {
+		return employeeRepository.findAll();
+	}
+
+	@Override
+	public Employee fetchEmployeeById (int id) {
+		return employeeRepository.findById(id).orElse(null); 
+	
+	}
+
+	@Override
+	public Employee updateEmployee(Employee employee) {
+		return employeeRepository.save(employee);
+	}
+
+	@Override
+	public String deleteEmployee(int id) {
+		employeeRepository.deleteById(id);
+		return "DELETED SUCCESSFULLY";
+	}
+
+}
